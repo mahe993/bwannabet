e0cc2b779fb4e92fc95b729bf6daa9f50473a37f
Ma He <qmahe@live.com>
2023-01-07 03:06:13 +0800

    added wallet endpoints

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ import FriendsController from "./controllers/friendsController.js";
 import FriendsRouter from "./routers/friendsRouter.js";
 import BetlinesController from "./controllers/betlinesController.js";
 import BetlinesRouter from "./routers/betlinesRouter.js";
+import WalletsController from "./controllers/walletsController.js";
+import WalletsRouter from "./routers/walletsRouter.js";
 
 //initialize env file
 dotenv.config();
@@ -32,12 +34,14 @@ const testsController = new TestsController(test);
 const usersController = new UsersController(user);
 const friendsController = new FriendsController(friend);
 const betlinesController = new BetlinesController(betline);
+const walletsController = new WalletsController(wallet);
 
 //initialize routers, routers passes in controllers, auth
 const testsRouter = new TestsRouter(testsController, checkJwt).routes();
 const usersRouter = new UsersRouter(usersController).routes();
 const friendsRouter = new FriendsRouter(friendsController).routes();
 const betlinesRouter = new BetlinesRouter(betlinesController).routes();
+const walletsRouter = new WalletsRouter(walletsController).routes();
 
 // logger
 app.use(morgan("dev"));
@@ -53,6 +57,7 @@ app.use("/tests", testsRouter);
 app.use("/users", checkJwt, usersRouter);
 app.use("/friends", checkJwt, friendsRouter);
 app.use("/betlines", betlinesRouter);
+app.use("/wallets", walletsRouter);
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
