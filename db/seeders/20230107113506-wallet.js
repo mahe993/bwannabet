94f447092902f13652363ea49c7f603b751537e3
Ma He <qmahe@live.com>
2023-01-09 16:37:24 +0800

    changed some seeder figures

--- db/seeders/20230107113506-wallet.js
+++ db/seeders/20230107113506-wallet.js
@@ -8,33 +8,26 @@ export default {
       "wallets",
       [
         {
-          on_hold: 5460,
+          on_hold: 4860,
           balance: 8000,
           user_id: "auth0|63aea209ac231151d6a252f7",
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          on_hold: 5460,
+          on_hold: 4860,
           balance: 8000,
           user_id: "auth0|63b1977b057a048253326ae8",
           created_at: new Date(),
           updated_at: new Date(),
         },
         {
-          on_hold: 5460,
+          on_hold: 4860,
           balance: 8000,
           user_id: "auth0|63b41dde057a04825332750b",
           created_at: new Date(),
           updated_at: new Date(),
         },
-        // {
-        //   on_hold: 5460,
-        //   balance: 8000,
-        //   user_id: "auth0|7C63b1977b057a048253326ae8",
-        //   created_at: new Date(),
-        //   updated_at: new Date(),
-        // },
       ],
       {}
     );
